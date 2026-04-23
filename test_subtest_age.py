33af7480000f5a628c9cc1922d887c894e8fdaa4
phonlapojmat-debug <phonlapoj.mat@student.mahidol.edu>
2026-04-23 05:33:30 +0000

    Add test cases

---
+++ test_subtest_age.py
@@ -0,0 +1,28 @@
+import unittest
+from age import categorize_by_age
+
+class TestAgeSubtests(unittest.TestCase):
+
+    def test_child_range(self):
+        for age in range(0, 10):
+            with self.subTest(age=age):
+                result = categorize_by_age(age)
+                print(f"{age} is considered as a child.")
+                self.assertEqual(result, "Child")
+
+    def test_adolescent_range(self):
+        for age in range(10, 19):
+            with self.subTest(age=age):
+                result = categorize_by_age(age)
+                print(f"{age} is considered as an adolescent.")
+                self.assertEqual(result, "Adolescent")
+
+    def test_golden_age_range(self):
+        for age in range(66, 151):
+            with self.subTest(age=age):
+                result = categorize_by_age(age)
+                print(f"{age} is considered as a golden age.")
+                self.assertEqual(result, "Golden age")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
